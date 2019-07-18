ef85a71d8abf1f6dfe7b8d993c75179501a2e3db
igorssds <igorsampaiosena@hotmail.com>
2019-07-18 17:52:13 -0300

    Inserindo os dados pessoais do paciente

--- src/main/java/com/mballem/curso/security/web/controller/PacienteController.java
+++ src/main/java/com/mballem/curso/security/web/controller/PacienteController.java
@@ -6,11 +6,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mballem.curso.security.domain.Paciente;
 import com.mballem.curso.security.domain.Usuario;
 import com.mballem.curso.security.service.PacienteService;
+import com.mballem.curso.security.service.UsuarioService;
 
 @Controller
 @RequestMapping("/pacientes")
@@ -19,6 +21,9 @@ public class PacienteController {
 	@Autowired
 	private PacienteService service;
 	
+	@Autowired
+	private UsuarioService usuarioService;
+	
 	@GetMapping("/dados")
 	public String cadastrar(Paciente paciente , ModelMap model, @AuthenticationPrincipal User user ) {
 		paciente = service.buscarPorUsuarioEmail(user.getUsername());
@@ -26,6 +31,20 @@ public class PacienteController {
 			paciente.setUsuario(new Usuario(user.getUsername()));
 		}
 		model.addAttribute("paciente" , paciente);
+		return "paciente/cadastro";
+	}
+
+	@PostMapping("/salvar")
+	public String salvar(Paciente paciente, ModelMap model , @AuthenticationPrincipal User user) {
+		Usuario u = usuarioService.buscarPorEmail(user.getUsername());
+		if(UsuarioService.isSenhaCorreta(paciente.getUsuario().getSenha(), u.getSenha())) {
+			paciente.setUsuario(u);
+			service.salvar(paciente);
+			model.addAttribute("sucesso", "Seus dados foram inseridos com sucesso.");
+		}else {
+			model.addAttribute("falha", "Sua senha não confere, tente novamente.");
+		}
+		
 		return "paciente/cadastro";
 	}
 	
